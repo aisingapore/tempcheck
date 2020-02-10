b8e1ab60b817046a52509f7a4626a46883f898bb
ningyuuu <ningyu321@gmail.com>
2020-02-10 14:44:25 +0800

    validate image

--- vishnu/serializers.py
+++ vishnu/serializers.py
@@ -1,3 +1,5 @@
+import imghdr
+
 from rest_framework import serializers
 from .models import Entry
 
@@ -13,11 +15,17 @@ class EntrySerializer(serializers.ModelSerializer):
         return value
 
     def validate_lat(self, value):
-        if lat < -90 or lat > 90:
+        if value < -90 or value > 90:
             raise serializers.ValidationError("Please enter valid coordinates: -90 < latitude < 90")
         return value
 
     def validate_long(self, value):
-        if lat < -180 or lat > 180:
+        if value < -180 or value > 180:
             raise serializers.ValidationError("Please enter valid coordinates: -180 < longitude < 180")
+        return value
+
+    def validate_file(self, value):
+        ext = imghdr.what(file=None, h=value.read())
+        if ext not in ["png", "jpg", "jpeg", "gif", "tif"]:
+            raise serializers.ValidationError("Please use one of the following file formats: png, jpg, jpeg, gif or tif.")
         return value
