5eef46a11aa3606a0161326d940b87f70114a499
ningyuuu <ningyu321@gmail.com>
2020-02-13 18:08:52 +0800

    updated entry form slider

--- frontend/src/components/EntryForm.vue
+++ frontend/src/components/EntryForm.vue
@@ -1,28 +1,34 @@
 <template>
-  <v-container fluid>
+  <v-container fluid id="entry-form">
     <v-form>
       <v-row justify="center">
         <v-col cols="12">
-          <v-row justify="center" class="pa-0 ma-0">
-            <v-col cols="1" class="pa-0 ma-0">
-              <v-icon>mdi-thermometer-lines</v-icon>
-            </v-col>
-          </v-row>
-          <v-row justify="center" class="mt-n3">
+          <v-row justify="center" class="mt-4">
             <v-col cols="10" xs="12">
               <v-slider
                 v-model="temperature"
                 min="35"
                 max="39"
                 thumb-label="always"
+                thumb-size="43"
                 step="0.1"
-                prepend-icon="mdi-minus"
-                append-icon="mdi-plus"
-              ></v-slider>
+              >
+                <template v-slot:prepend>
+                  <v-icon @click="decrement">
+                    mdi-minus
+                  </v-icon>
+                </template>
+
+                <template v-slot:append>
+                  <v-icon @click="increment">
+                    mdi-plus
+                  </v-icon>
+                </template>
+              </v-slider>
             </v-col>
           </v-row>
-          <!-- The vuetify file input may not have Camera capture capability -->
-          <v-row class="mt-n3">
+
+          <v-row class="mt-n6 pb-2">
             <v-col>
               <vue-picture-input
                 height="300"
@@ -39,12 +45,15 @@
               />
             </v-col>
           </v-row>
+
           <v-row justify="center" class="pa-1">
             <b>{{ location }}</b>
           </v-row>
+
           <v-row justify="center" class="pa-1">
             <b>{{ currentTime }}</b>
           </v-row>
+
           <v-row justify="center" class="mt-4">
             <v-btn color="success" @click="submit">
               Submit
@@ -87,6 +96,9 @@ export default {
     locationMsg: "Getting location"
   }),
   methods: {
+    decrement() {
+      this.temperature -= 0.1;
+    },
     fileUpload() {
       console.log("fileInput", this.$refs.fileInput);
       this.file = this.$refs.fileInput.file;
@@ -117,6 +129,9 @@ export default {
       } else {
         console.log("Geo Location not supported by browser");
       }
+    },
+    increment() {
+      this.temperature += 0.1;
     },
     // function that retrieves the position
     showPosition(position) {
@@ -176,23 +191,9 @@ export default {
   }
 };
 </script>
-<style scoped>
-form > input {
-  border: 1px solid grey;
-  margin: 5px 0px;
-}
-
-.form-input {
-  background-color: linen;
-  width: 60%;
-  display: block;
-  margin: 10px auto;
-  padding: 8px 20px;
-}
-
-.btn-submit {
-  border: 1px solid darkgrey;
-  margin-top: 10px;
-  margin-bottom: 5px;
+<style>
+#entry-form .v-slider__thumb-label {
+  font-size: 1em !important;
+  font-weight: bold;
 }
 </style>
