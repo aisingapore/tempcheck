6cb50ce137ecf262df59661c212d00536c4e2ece
Wei Yih <weiyih@aisingapore.org>
2020-02-13 09:55:02 +0800

    Add knox dependency and pylint disable no member for User model

--- vishnu/models.py
+++ vishnu/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
 
 # Ensure only unique emails in DB
-User._meta.get_field('email')._unique = True 
+# pylint: disable=no-member
+User._meta.get_field('email')._unique = True
