bbcb1582a739aba6d22e817e2c8726e35bef8370
Kian Boon Koh <kianboonkoh@aisingapore.org>
2020-04-02 23:37:23 +0800

    implement custom filter to enable page back of previous 7 days

--- vishnu/admin.py
+++ vishnu/admin.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import pytz
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.utils.encoding import force_text
 from django.utils.html import format_html
 
 from .models import Entry, UserAuth
@@ -11,14 +12,33 @@ PST = pytz.timezone('Singapore')
 
 
 class DateIndexFilter(admin.SimpleListFilter):
-    title = 'Date Index'
+    title = 'Date'
     parameter_name = 'date_index'
 
     def lookups(self, request, model_admin):
-        return ([])
+        return (
+            ('1', 'Previous'),
+            ('-1', 'Next'),
+            ('today', 'Today')
+        )
 
     def queryset(self, request, queryset):
         return queryset
+
+    def choices(self, changelist):
+        '''Override to remove the "All" Option
+        Refer to https://stackoverflow.com/questions/53821727/django-admin-list-filter-remove-all-option
+        '''
+        yield {
+            'selected': self.value() is None,
+            'query_string': changelist.get_query_string({}, [self.parameter_name])
+        }
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == force_text(lookup),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}, []),
+                'display': title,
+            }
 
 
 class Day:
@@ -26,6 +46,7 @@ class Day:
     Refer to https://stackoverflow.com/questions/19886218/django-admin-short-description-as-callable
     for additional information on how this was implemented
     '''
+
     def __init__(self, date):
         self.date = date
 
@@ -79,24 +100,31 @@ class Day:
 class EntryAdmin(admin.ModelAdmin):
     actions = None
     list_display = ['id', 'username'] + [f'day_{i}' for i in range(7)]
-    list_per_page = 2
     list_filter = (DateIndexFilter, )
     list_display_links = None
     search_fields = ['id', 'username']
+    date_index = 0
     filter_dates = [datetime.now(PST).date() - timedelta(days=i)
                     for i in range(6, -1, -1)]
 
     def get_queryset(self, request):
-        print(request.GET)
-        date_index = request.GET.get('date_index', 0)
-        print(date_index)
-        self.update_filter_dates(date_index)
+        date_index_param = request.GET.get('date_index', 0)
+        if date_index_param == 'today':
+            self.date_index = 0
+        else:
+            self.date_index = max(self.date_index + int(date_index_param), 0)
+        self.update_filter_dates()
         qs = User.objects.all().order_by('id')
         return qs
 
-    def update_filter_dates(self, date_index: int):
+    def changelist_view(self, request, extra_context=None):
+        '''Override to change page title '''
+        extra_context = {'title': 'Temperature Readings'}
+        return super(EntryAdmin, self).changelist_view(request, extra_context=extra_context)
+
+    def update_filter_dates(self):
         self.filter_dates = [
-            datetime.now(PST).date() - timedelta(days=i+int(date_index)*7) for i in range(6, -1, -1)
+            datetime.now(PST).date() - timedelta(days=i+int(self.date_index)*7) for i in range(6, -1, -1)
         ]
 
     def get_property(self, index: int):
