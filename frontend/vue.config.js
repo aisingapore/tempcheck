1eba20744aef0a60f2adbe9d57cc32be66a08162
Wei Yih <weiyih@aisingapore.org>
2020-04-12 13:15:22 +0800

    Add workbox option to clean stale cache

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -12,6 +12,9 @@ module.exports = {
       favicon32: "static/icons/favicon-32x32.png",
       favicon16: "static/icons/favicon-16x16.png",
       appleTouchIcon: "static/icons/apple-touch-icon-152x152.png"
+    },
+    workboxOptions: {
+      cleanupOutdatedCaches: true
     }
   },
 
